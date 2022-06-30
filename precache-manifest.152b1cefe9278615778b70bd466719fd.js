0ff84e6607bd787c0a7c49fda624c4ceb782cbbb
izuutech <izutechsofficial@gmail.com>
2022-06-30 11:10:57 +0100

    Updates

---
+++ precache-manifest.152b1cefe9278615778b70bd466719fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0c808645c9e06fe264aac698d5829a",
+    "url": "/reacttodoapp/index.html"
+  },
+  {
+    "revision": "05cfee5b04276aa8c788",
+    "url": "/reacttodoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/reacttodoapp/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodoapp/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05cfee5b04276aa8c788",
+    "url": "/reacttodoapp/static/js/main.d9d78f63.chunk.js"
+  },
+  {
+    "revision": "ebbcd52153a4324d828c",
+    "url": "/reacttodoapp/static/js/runtime-main.128b8f99.js"
+  }
+]);
